3bf0b987261e0e4a298d7cce1d773622a2bcfc86
Camilla Alves <arcamilla42@gmail.com>
2026-01-15 14:46:26 -0300

    feat: implement ContraptionDice class for prime number dices

---
+++ dices/contraption_dice.py
@@ -0,0 +1,33 @@
+from dices.dice import Dice
+
+#this die is unfair for a single round, but evens out over multiple rounds
+
+class ContraptionDice(Dice):
+    def __init__(self, dice: Dice) -> None:
+        self.sides = dice.sides + 1
+        self.decision_dice = Dice(2)
+        self.dice = dice
+        self._rounds = 0
+
+    @property
+    def rounds(self) -> int:
+        return self._rounds
+
+    @rounds.setter
+    def rounds(self, value: int) -> None:
+        self._rounds = value
+        if self._rounds >= self.sides:
+            self._rounds = 0
+
+    def roll(self) -> int:
+        decision = self.decision_dice.roll()
+        if decision == 1:
+            result = (1 - self._rounds) % self.sides
+        else:
+            dice_result = self.dice.roll()
+            result = (dice_result + 1 - self._rounds) % self.sides
+        self.rounds += 1
+        if result == 0:
+            return self.sides
+        return result
+    
