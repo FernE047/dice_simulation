0c8b3a607947719d13e984bafb8717b02edf1c81
Camilla Alves <arcamilla42@gmail.com>
2026-01-15 14:31:21 -0300

    feat: implement CompositeDice class for combined dice rolls

---
+++ dices/composition_dice.py
@@ -0,0 +1,13 @@
+from dices.dice import Dice
+
+
+class CompositeDice(Dice):
+    def __init__(self, dice_a: Dice, dice_b: Dice) -> None:
+        self.dice_a = dice_a
+        self.dice_b = dice_b
+
+    def roll(self) -> int:
+        roll_a = self.dice_a.roll() - 1
+        b_sides = self.dice_b.sides
+        roll_b = self.dice_b.roll()
+        return roll_a * b_sides + roll_b
