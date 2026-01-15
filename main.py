44b4af904d928033304b5eee8dd9592dc4d501ae
Camilla Alves <arcamilla42@gmail.com>
2026-01-15 14:25:48 -0300

    feat: add ModDice instances for additional dice rolls

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dice import Dice
+from mod_dice import ModDice
 
 # 1. dices we have available to use
 
@@ -8,3 +9,10 @@ dice_8 = Dice(8)
 dice_10 = Dice(10)
 dice_12 = Dice(12)
 dice_20 = Dice(20)
+
+# 2. dice we mod
+
+dice_1 = ModDice(dice_4, 1)
+dice_2 = ModDice(dice_4, 2)
+dice_3 = ModDice(dice_6, 3)
+dice_5 = ModDice(dice_10, 5)
