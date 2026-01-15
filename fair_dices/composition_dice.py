3f4146935b927a480f03b194e938e067355511e0
Camilla Alves <arcamilla42@gmail.com>
2026-01-15 16:40:57 -0300

    feat: new MultiDice that combines values based on number systems

--- fair_dices/composition_dice.py
+++ fair_dices/composition_dice.py
@@ -1,7 +1,32 @@
 from fair_dices.dice import Dice
 
 
-class DuoDice(Dice):
+class MultiDice(Dice):
+    def __init__(self, dices: list[Dice]) -> None:
+        self.sides = 1
+        for dice in dices:
+            self.sides *= dice.sides
+        self.dices = dices
+
+    def roll(self) -> int:
+        result = 0
+        multiplier = 1
+        for dice in self.dices:
+            roll = dice.roll() - 1
+            result += roll * multiplier
+            multiplier *= dice.sides
+        return result + 1
+
+    def __str__(self) -> str:
+        dices_str = ", ".join(f"d{dice.sides}" for dice in self.dices)
+        return f"MultiDice({dices_str}) = d{self.sides}"
+
+    def explain(self) -> str:
+        dices_explain = ", ".join(dice.explain() for dice in self.dices)
+        return f"MultiDice({dices_explain})"
+
+
+class DuoDice(Dice):  # it's a multi dice with two dices. here for legacy reasons
     def __init__(self, dice_a: Dice, dice_b: Dice) -> None:
         self.sides = dice_a.sides * dice_b.sides
         self.dice_a = dice_a
@@ -14,9 +39,7 @@ class DuoDice(Dice):
         return roll_a * b_sides + roll_b
 
     def __str__(self) -> str:
-        return (
-            f"DuoDice(d{self.dice_a.sides}, d{self.dice_b.sides}) = d{self.sides}"
-        )
+        return f"DuoDice(d{self.dice_a.sides}, d{self.dice_b.sides}) = d{self.sides}"
 
     def explain(self) -> str:
         return f"DuoDice({self.dice_a.explain()}, {self.dice_b.explain()})"
